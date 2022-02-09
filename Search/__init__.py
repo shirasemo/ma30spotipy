2b9b8ceb8a8003eb02eb00215c42e3399d3093df
Shira Semo <shirasemo3@gmail.com>
2022-02-09 18:46:18 +0200

    added top_tracks function

--- Search/__init__.py
+++ Search/__init__.py
@@ -16,3 +16,25 @@ def artist_albums(artist_id):
     except AttributeError:
         raise ArtistNotFound
 
+
+def artist_top_tracks(artist_id):
+    try:
+        albums_id = Models.artists.get(artist_id).albums_id
+    except AttributeError:
+        raise ArtistNotFound
+    for id in albums_id:
+        album = Models.albums.get(id)
+        tracks = album.tracks_ids
+        result = []
+        for track in tracks:
+            pos = Models.tracks.get(track)
+            result.append(pos)
+    return [track.name for track in sorted(result, key=lambda t: t.popularity, reverse=True)]
+
+
+def main():
+    print(artist_top_tracks('5BtHciL0e0zOP7prIHn3pP'))
+
+
+if __name__ == "__main__":
+    main()
