4ddae6ed6176d0b50de3447f9c632772b0f283b2
Shira Semo <shirasemo3@gmail.com>
2022-02-09 09:37:40 +0200

    added Album class

--- Models/MusicModels.py
+++ Models/MusicModels.py
@@ -7,3 +7,12 @@ class Artist:
         else:
             self.albums = albums
 
+
+class Album:
+    def __init__(self, id, name, songs=None):
+        self.id = id
+        self.name = name
+        if songs is None:
+            self.songs = []
+        else:
+            self.songs = songs
