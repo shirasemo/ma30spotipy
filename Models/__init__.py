7b90706f87b7e3448fb61f1889457efde4b91d52
Shira Semo <shirasemo3@gmail.com>
2022-02-09 11:52:10 +0200

    moved classes to __init__ file

---
+++ Models/__init__.py
@@ -0,0 +1,27 @@
+class Artist:
+    def __init__(self, id, name, albums=None):
+        self.id = id
+        self.name = name
+        if albums is None:
+            self.albums = []
+        else:
+            self.albums = albums
+
+
+class Album:
+    def __init__(self, id, name, songs=None):
+        self.id = id
+        self.name = name
+        if songs is None:
+            self.songs = []
+        else:
+            self.songs = songs
+
+
+class Track:
+    def __init__(self, album, artists, id, name, popularity):
+        self.album = album
+        self.artists = artists
+        self.id = id
+        self.name = name
+        self.popularity = popularity
