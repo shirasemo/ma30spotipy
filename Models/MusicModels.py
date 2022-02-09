2f1df0ee91d39613f885afa4692a4168a50bdb0c
Shira Semo <shirasemo3@gmail.com>
2022-02-09 09:50:27 +0200

    added track class

--- Models/MusicModels.py
+++ Models/MusicModels.py
@@ -16,3 +16,13 @@ class Album:
             self.songs = []
         else:
             self.songs = songs
+
+
+class Track:
+    def __init__(self, album, artists, id, name, popularity):
+        self.album = album
+        self.artists = artists
+        self.id = id
+        self.name = name
+        self.popularity = popularity
+        
