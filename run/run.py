ce881ea1084d1a697d3154efaacc18a2094a1887
Shira Semo <shirasemo3@gmail.com>
2022-02-10 15:36:22 +0200

    edited run file

--- run/run.py
+++ run/run.py
@@ -1,11 +1,28 @@
+from consolemenu import *
+from consolemenu.items import *
+
 import extractor
+from search.search_methods import *
 from resources.config import configuration
+from users.users_models import User
 
+extractor.parse_files(configuration.tracks_metadata_path)
+configuration.read_configuration()
 
-def main():
-    extractor.parse_files(configuration.tracks_metadata_path)
-    configuration.read_configuration()
+configuration.user = User('username', 'password')
 
+menu = ConsoleMenu("spotipy")
 
-if __name__ == "__main__":
-    main()
+menu_item = MenuItem("search")
+
+search_artists = FunctionItem("show artists", artists)
+search_artist_top_tracks = FunctionItem('artist top tracks', artist_top_tracks)
+search_artist_albums = FunctionItem('artist album', artist_albums)
+search_track_in_album = FunctionItem('tracks in album', track_in_album)
+
+menu.append_item(search_artists)
+menu.append_item(search_artist_albums)
+menu.append_item(search_artist_top_tracks)
+menu.append_item(search_track_in_album)
+
+menu.show()
