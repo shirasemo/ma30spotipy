61a9491d17fe6a811706ad5dd88f00f26cf5d32d
Shira Semo <shirasemo3@gmail.com>
2022-02-10 09:30:24 +0200

    edited user class

--- Users/__init__.py
+++ Users/__init__.py
@@ -1,14 +1,31 @@
-from Exceptions import PlaylistAlreadyExists
+from Exceptions import *
+
+users = {}
 
 
 class User:
-    def __init__(self, name, type='Free'):
-        self.name = name
+    def __init__(self, username, password, type='Free'):
+        self.username = username
+        self.password = password
         self.type = type
         self.playlists = {}
+        users.update({username: self})
 
-    def create_playlist(self, name, tracks):
-        if self.playlists.get(name) is None:
-            self.playlists.update({name: tracks})
+    def create_playlist(self, name):
+        if self.type == 'Free' and len(self.playlists) <= 5:
+            if self.playlists.get(name) is None:
+                self.playlists.update({name: []})
+                users.update({self.username: self})
+            else:
+                raise PlaylistAlreadyExists
         else:
-            raise PlaylistAlreadyExists
+            raise NotAPremiumUser
+
+    def add_track(self, playlist_name, track):
+        user = users.get(self.username)
+        playlist = user.playlists.get(playlist_name)
+        if user.type == 'Free':
+            if len(playlist) <= 20:
+                playlist.append(track)
+            else:
+                raise NotAPremiumUser
