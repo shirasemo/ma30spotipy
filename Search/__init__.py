32df581fa1b7e83f8b11d9815768ca3eaa620d0e
Shira Semo <shirasemo3@gmail.com>
2022-02-10 08:58:59 +0200

    added a decorator

--- Search/__init__.py
+++ Search/__init__.py
@@ -1,14 +1,31 @@
+import functools
+
 import Models
 import Extractor
+import Users
 from Exceptions import *
 
 Extractor.parse_files(r"C:\Users\shira\Downloads\drive-download-20220209T075512Z-001.zip")
+user = Users.User('shira', 'semo')
 
 
+def limit_search_results(func):
+
+    @functools.wraps(func)
+    def inner(*args):
+        if user.type == 'Free':
+            return func(*args)[:5:]
+        return func(*args)
+
+    return inner
+
+
+@limit_search_results
 def artists():
     return [artist.name for artist in Models.artists.values()]
 
 
+@limit_search_results
 def artist_albums(artist_id):
     try:
         albums_id = Models.artists.get(artist_id).albums_id
@@ -17,6 +34,7 @@ def artist_albums(artist_id):
         raise ArtistNotFound
 
 
+@limit_search_results
 def artist_top_tracks(artist_id):
     try:
         albums_id = Models.artists.get(artist_id).albums_id
@@ -32,6 +50,7 @@ def artist_top_tracks(artist_id):
     return [track.name for track in sorted(result, key=lambda t: t.popularity, reverse=True)]
 
 
+@limit_search_results
 def track_in_album(album_id):
     try:
         album = Models.albums.get(album_id)
